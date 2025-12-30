9514bb5efd0678f5c03b7a5d490ba41cacc2c9b3
Louis-fortress <louis.adu@fortressaisolutions.com>
2025-12-30 11:11:35 +0000

    Added airframes; 22001_gazebo-classic_uuv_reconbot

--- src/modules/simulation/simulator_mavlink/sitl_targets_gazebo-classic.cmake
+++ src/modules/simulation/simulator_mavlink/sitl_targets_gazebo-classic.cmake
@@ -104,6 +104,7 @@ if(gazebo_FOUND)
 		typhoon_h480
 		uuv_bluerov2_heavy
 		uuv_hippocampus
+		uuv_reconbot
 	)
 
 	set(worlds
@@ -117,6 +118,7 @@ if(gazebo_FOUND)
 		warehouse
 		windy
 		yosemite
+		fortress_pool
 	)
 
 	# find corresponding airframes
